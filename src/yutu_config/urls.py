fc3e2f69cb90bc267dfd46d500e0530c56f25356
Kai <ttkhai93@gmail.com>
2026-04-15 21:57:42 +0700

    fix css on local

--- src/yutu_config/urls.py
+++ src/yutu_config/urls.py
@@ -29,4 +29,4 @@ urlpatterns = [
 # Serve media files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # Static files are automatically served by Django from STATICFILES_DIRS when DEBUG=True
